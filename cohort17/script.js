5c060f0c430f24c9b1cf6bdb7905eece4ef7cbf9
john <johnojochegbegodwin@yahoo.com>
2021-05-04 12:16:02 +0100

    birthdayapp

---
+++ cohort17/script.js
@@ -0,0 +1,78 @@
+	
+/*function multiply (a,b){ 
+	var r = a*b
+	alert(r)
+}
+
+	var t =multiply(10,2)
+
+alert(t);
+document.write(t)*/
+
+
+
+
+// for (var i = 1; i <= 5; i=i+1) {
+// alert ('udama')
+// }
+
+
+
+// for (var i =1; i <=10; i=i+1){
+
+// 	document.write(i)
+// }
+
+
+
+
+// for( i = 1; i <= 10; i=i+2){
+// document.write(i)
+// }
+
+
+// for(i=1;i<=10; i=i+1){
+
+
+// if(i%2 !=0){
+// 	document.write(i)
+// }
+// }
+
+
+
+ for (var i=1;  i-1 < 100; i--){
+ 	document.write (i)
+ }
+
+
+
+//  x = 1;
+// while (x <10){
+// 	alert('the number is' + x)
+// 	x = x +1;
+// }
+
+// document.write(<h1>'+i+'<h1>)
+
+
+// document.write('<li>'+l +'<li>');
+
+// // else if(a<40:failed)
+// // document.write('grade-E')
+
+// <script src="script.js">
+	
+// 	for (var i = 0; i < 10; i++){
+// 		document.get(i)
+// 	}
+
+
+
+
+
+// for (var i =10; i >=1; i=i-1){
+
+// 	document.write(i)
+// }
+
